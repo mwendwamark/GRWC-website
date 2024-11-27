d4ccf7ec1e3c463024799a1521652c6aa8273e61
mwendwamark <mwendwamark310@gmail.com>
2024-11-27 20:00:17 +0300

    create the mission and vision section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,14 +8,15 @@ import { HeroData } from "../../assets/HeroImages/HeroData"; // Import your Hero
 import "./Home.css";
 import { NavLink } from "react-router-dom";
 import { LiaLongArrowAltRightSolid } from "react-icons/lia";
+import { MdTrackChanges } from "react-icons/md";
+import { FaRegLightbulb } from "react-icons/fa";
 import img from "../../assets/HeroImages/image3.jpg";
 
 const Home = () => {
   return (
-    <div className="home section">
+    <main className="home section">
       <div className="home-container">
-        <div className="hero-section container">
-          {/* Title */}
+        <section className="hero-section container">
           <div className="hero-title">
             <h1>
               GOSPEL REVIVAL <br /> <span>WAVE CHURCH</span>
@@ -23,7 +24,6 @@ const Home = () => {
             <p>Preaching Jesus Christ, the desire of all nations</p>
           </div>
 
-          {/* Swiper Slider */}
           <Swiper
             slidesPerView={1}
             spaceBetween={0}
@@ -56,6 +56,7 @@ const Home = () => {
                       src={item.image}
                       alt={`Slide ${item.id}`}
                       className="image-slide"
+                      loading="lazy"
                     />
                   )}
                   <div className="slide-text">
@@ -66,13 +67,13 @@ const Home = () => {
               </SwiperSlide>
             ))}
           </Swiper>
-        </div>
+        </section>
 
-        <div className="church-history-section container section">
-          <div className="history-headers">
+        <section className="church-history-section container section">
+          <header className="history-headers">
             <h2 className="small-header">About us</h2>
             <h1 className="big-header">Get to know us</h1>
-          </div>
+          </header>
 
           <div className="church-history-contents">
             <div className="church-history-image-container">
@@ -100,14 +101,48 @@ const Home = () => {
                 spiritual growth and community transformation, the church
                 continues to embrace the future with faith and purpose.
               </p>
+
               <NavLink to="/about" className="read-more-btn">
                 Read more <LiaLongArrowAltRightSolid className="purple-icon" />
               </NavLink>
             </div>
           </div>
-        </div>
+        </section>
+
+        <section className="mission-and-vision container section">
+          <header className="mission-and-vision-headers">
+            <h2 className="small-header-h3">OUR MISSION & VISION</h2>
+          </header>
+          <div className="mission-and-vision-contents">
+            <div className="church-mission">
+              <header className="church-mission-header">
+                <h2>Church Mission</h2>{" "}
+                <MdTrackChanges className="mission-and-vision-icon" />
+              </header>
+              <div className="church-mission-text">
+                “To spread the love and truth of Jesus Christ, fostering a
+                community where every individual can experience spiritual
+                growth, transformation, and a renewed purpose in life. “
+              </div>
+            </div>
+
+            <div className="church-vision">
+              <header className="church-vision-header">
+                <h2>Church Vision</h2>
+                <FaRegLightbulb className="mission-and-vision-icon" />
+              </header>
+              <div className="church-vision-text">
+                “To be a light in Kenya and beyond, empowering individuals and
+                families to live out their faith boldly, impacting communities
+                with compassion, and creating lasting change through God’s
+                love.“
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section"></section>
       </div>
-    </div>
+    </main>
   );
 };
 
