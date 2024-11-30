2ef6913fa0fd6101e58d11e82375e9794c3dc7a1
mwendwamark <mwendwamark310@gmail.com>
2024-11-30 09:34:20 +0300

    create the events section and make it responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -241,6 +241,19 @@ const Home = () => {
             ))}
           </Swiper>
         </section>
+
+        <section className="events-section section">
+          <header className="events-headers container">
+            <h2 className="small-header">EVENTS</h2>
+            <h1 className="big-header">UPCOMING EVENTS</h1>
+          </header>
+          <div className="events-contents">
+            <p>Click the button to show the upcoming events</p>
+            <NavLink to="/events">
+              <p>Upcoming events</p> <LiaLongArrowAltRightSolid />
+            </NavLink>
+          </div>
+        </section>
       </div>
     </main>
   );
