e8f10dd96acdcc1000caadcc321e456d401ea1dc
mwendwamark <mwendwamark310@gmail.com>
2025-01-06 08:38:04 +0300

    display construction message

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,30 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
+import Branches from "./pages/Branches/Branches";
+import Sermons from "./pages/Sermons/Sermons";
+import Ministries from "./pages/Ministries/Ministries";
+import Events from "./pages/Events/Events";
+import About from "./pages/About/About";
+import Contacts from "./pages/Contacts/Contacts";
+import Give from "./pages/Give/Give";
+
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} ></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/branches" element={<Branches />}></Route>
+        <Route path="/sermons" element={<Sermons />}></Route>
+        <Route path="/events" element={<Events />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/ministries" element={<Ministries />}></Route>
+        <Route path="/contact" element={<Contacts />}></Route>
+        <Route path="/donate" element={<Give />}></Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 };
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import image from "../../assets/constructions.jpeg";
+
+const About = () => {
+  return (
+    <div>
+      <div className='container construction-div section'>
+        <h1>Page under construction</h1>
+        <img src={image} alt="" className='construction-img' />
+      </div>
+    </div>
+  );
+}
+
+export default About
